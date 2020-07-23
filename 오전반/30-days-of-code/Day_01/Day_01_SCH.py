b98a0f84a215d31ab159fae9ddf9aa12f118f41f
cogns5796 <68671966+cogns5796@users.noreply.github.com>
2020-07-23 10:36:12 +0900

    Create Day_01_SCH.py

---
+++ 오전반/30-days-of-code/Day_01/Day_01_SCH.py
@@ -0,0 +1,21 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+i2 = int(input()) # read int
+d2 = float(input()) # read double 
+s2 = input() # read string
+
+# print summed and concatenated values
+print(i + i2)
+print(d + d2)
+print(s + s2)
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+
