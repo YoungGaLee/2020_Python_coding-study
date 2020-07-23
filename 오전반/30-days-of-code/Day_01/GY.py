8eda2d87f587152a8ae43f9665164ad26c19d370
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-07-23 10:15:00 +0900

    Create GY.py

---
+++ 오전반/30-days-of-code/Day_01/GY.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+I =int(input()) #문자열로 받은 값을 int형을 통해 숫자로 바꿈.(정수)
+D = float(input()) #소수는 실수형인 float로 받음.
+S = str(input()) #str은 문자형을 의미. 이미 문자열로 받기에 안써도 됨.
+
+print(i + I) #'+'연산
+print(d + D) # 합
+
+print(s + S) #두 문자열을 합칠때도 사용
