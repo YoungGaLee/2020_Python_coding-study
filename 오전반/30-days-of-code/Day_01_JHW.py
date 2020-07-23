636e34273577d3cb76c8ea11896c885e2dab5516
wnghddnr <68671947+wnghddnr@users.noreply.github.com>
2020-07-23 10:45:26 +0900

    Create Day_01_JHW.py

---
+++ 오전반/30-days-of-code/Day_01_JHW.py
@@ -0,0 +1,13 @@
+
+# Declare second integer, double, and String variables.
+a=int(input())
+b=float(input())
+c=str(input())
+# Read and save an integer, double, and String to your variables.
+print(a+i)
+# Print the sum of both integer variables on a new line.
+print(b+d)
+# Print the sum of the double variables on a new line.
+print(s+c)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
