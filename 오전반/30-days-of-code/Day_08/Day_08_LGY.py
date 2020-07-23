47a64972e3bbe1de179259c64285fa7f81ee8a61
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-07-23 10:43:05 +0900

    Create Day_08_LGY.py

---
+++ 오전반/30-days-of-code/Day_08/Day_08_LGY.py
@@ -0,0 +1,16 @@
+
+ 
+N = int(input()) 
+d = dict() 
+for _ in range(0, N): 
+    name, number = input().split() 
+    d[name] = number 
+ 
+ 
+for _ in range(0, N): 
+    name = input() 
+    if name in d: 
+        print("{}={}".format(name, d[name])) 
+    else: 
+        print("Not found") 
+#다시
