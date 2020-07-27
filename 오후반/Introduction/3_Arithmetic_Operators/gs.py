161641d64d44114bdcc1ff3d9571bc483a90846d
Leegeonsu <68536274+Leegeonsu@users.noreply.github.com>
2020-07-27 17:28:22 +0900

    Create gs.py

---
+++ 오후반/Introduction/3_Arithmetic_Operators/gs.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+ a = int(input())
+ b = int(input())
+if (a>=1 and a<=10**10):
+ if (b>=1 and b<=10**10):
+     x = a+b
+     y = a-b
+     z = a*b
+     print (x)    
+     print (y)
+     print (z)
