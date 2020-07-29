2f09f1ac69bd4d5109210ed430fe0394d836d0d4
YoungGaLee <43063980+YoungGaLee@users.noreply.github.com>
2020-07-30 06:11:44 +0900

    Create  Day_07_LGY.py

---
+++ 오전반/30-days-of-code/Day_07/Day_07_LGY.py
@@ -0,0 +1,13 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = input().rstrip().split()
+    print(" ".join(arr[::-1]))
