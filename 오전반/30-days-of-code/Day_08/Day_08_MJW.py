e78fd01b25365cd7867874da25a62610d41b9105
treejw <42428487+treejw@users.noreply.github.com>
2020-07-30 16:50:20 +0900

    Update Day_08_MJW.py

--- 오전반/30-days-of-code/Day_08/Day_08_MJW.py
+++ 오전반/30-days-of-code/Day_08/Day_08_MJW.py
@@ -7,12 +7,13 @@ for _ in range(n):
     name, phone_number = x[0], x[1]
     phone_book[name] = phone_number
 
-for _ in range(n):
-    query_name = input()
-    if query_name in phone_book:
-        phone_number = phone_book[query_name]
-        print('{}={}'.format(query_name, phone_number))
-    else:
-        print('Not found')
-
-    
+for _ in range(10**5):
+    try:
+        query_name = input()
+        if query_name in phone_book:
+            phone_number = phone_book[query_name]
+            print('{}={}'.format(query_name, phone_number))
+        else:
+            print('Not found')
+    except:
+        break
