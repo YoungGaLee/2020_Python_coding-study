0a9378691880067bf49aec7ccb1a8bbe1d4b655a
Nayoung1124 <53847442+Nayoung1124@users.noreply.github.com>
2020-08-06 10:52:08 +0900

    Update 4_Finding_the_percentage_YNY.py

--- 오전반/Basic-Data-Types/4_Finding_the_percentage/4_Finding_the_percentage_YNY.py
+++ 오전반/Basic-Data-Types/4_Finding_the_percentage/4_Finding_the_percentage_YNY.py
@@ -1,31 +1,12 @@
 if __name__ == '__main__':
-
-    tmp = []
-    studentList = []
-    for _ in range(int(input())):
-        name = input()
-        score = float(input())
-        tmp.append(name)
-        tmp.append(score)
-        studentList.append(tmp)
-        tmp = []
-
-    SortedStudentList = sorted(studentList, key=lambda x: x[1])
-
-    for i in range(len(SortedStudentList) - 1):
-        MinScore = SortedStudentList[i][1]
-        if SortedStudentList[i][1] == SortedStudentList[i + 1][1]:
-            SecondMinScore = SortedStudentList[i + 1][1]
-            continue
-        else:
-            SecondMinScore = SortedStudentList[i + 1][1]
-            break
-
-    ans = []
-    for i in range(len(SortedStudentList)):
-        if SortedStudentList[i][1] == SecondMinScore:
-            ans.append(SortedStudentList[i][0])
-
-    ans.sort()
-    for i in ans:
-        print(i)
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    q_score=student_marks[query_name]
+    ans=sum(q_score)/len(q_score)
+    print('%.2f' %ans)
