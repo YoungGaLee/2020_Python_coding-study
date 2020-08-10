4a3e25634d8d71338e3fe53791fea59b4604a5be
Nayoung1124 <53847442+Nayoung1124@users.noreply.github.com>
2020-08-10 15:17:12 +0900

    Create  5_Find_a_string_YNY.py

---
+++ 오후반/Strings/5_Find_a_string/5_Find_a_string_YNY.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    c = 0
+    for i in range(0, len(string)): 
+        if string[i]==sub_string[0]:
+            if string[i:i+len(sub_string)] == sub_string:
+                c += 1
+
+    return c
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
