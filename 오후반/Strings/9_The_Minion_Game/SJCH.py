d359f0aad27dc8848dd2726b1f42f8bb6fbf7cd2
socity11 <68587567+socity11@users.noreply.github.com>
2020-08-24 17:12:26 +0900

    Create SJCH.py

---
+++ 오후반/Strings/9_The_Minion_Game/SJCH.py
@@ -0,0 +1,20 @@
+def minion_game(string):
+    # your code goes here
+    player1 = 0;
+    player2 = 0;
+    str_len = len(string)
+    for i in range(str_len):
+        if s[i] in "AEIOU":
+            player1 += (str_len)-i
+        else : 
+            player2 += (str_len)-i
+
+    if player1 > player2:
+        print("Kevin", player1)
+    elif player1 < player2:
+        print("Stuart", player2)
+    elif player1 == player2:
+        print("Draw")
+    else : 
+        print("Draw")
+
