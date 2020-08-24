96d606d9de2393bb404fa8aba5b182974eb072d3
socity11 <68587567+socity11@users.noreply.github.com>
2020-08-24 17:31:15 +0900

    Update SJCH.py

--- 오후반/Strings/8_Strong_Password/SJCH.py
+++ 오후반/Strings/8_Strong_Password/SJCH.py
@@ -10,6 +10,22 @@ import sys
 def minimumNumber(n, password):
     # Return the minimum number of characters to make the password strong
 
+    password = set(password)
+    cnt = [0]*4
+    for c in password:
+        if c.isdigit():
+            cnt[0] = 1
+        elif c.islower():
+            cnt[1] = 1
+        elif c.isupper():
+            cnt[2] =1
+        else :
+            cnt[3] = 1
+        if sum(cnt) ==4:
+            break
+    return max(4 -sum(cnt), 6-n)
+
+
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
