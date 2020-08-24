f2de07efcd3037770a7755b9ba82b0b39e0b5017
socity11 <68587567+socity11@users.noreply.github.com>
2020-08-24 17:10:43 +0900

    Create SJCH.py

---
+++ 오후반/Strings/8_Strong_Password/SJCH.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumNumber function below.
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()
